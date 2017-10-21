44720d3c3d6f2c41975a48f03251d1fc16f2c227
jinhyuk <jinhyuk.seo@gmail.com>
2017-10-21 18:15:29 +0400

    Finally Completed. But I could't decide the file.

--- resize.py
+++ resize.py
@@ -2,15 +2,16 @@
 
 import cv2
 import os
+import glob
 
-image_names = ["galaxy.jpg", "kangaroos-rain-australia_71370_990x742.jpg", "Lighthouse.jpg", "Moon sinking, sun rising.jpg"]
+image_names = glob.glob("sample-images/*.jpg")
 
-path_resized = "resized/"
+path_resized = "resized/sample-images/"
 if not os.path.exists(path_resized):
     os.makedirs(path_resized)
 
 for image in image_names:
-    print("sample-images/"+image)
-    img = cv2.imread("sample-images/"+image, 1)
+    print(image)
+    img = cv2.imread(image, 1)
     resized_image = cv2.resize(img, (100, 100))
     cv2.imwrite("resized/"+image, resized_image)
